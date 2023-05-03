73beacc30f752d7105e03e19fa8a2dddb46ceeaf
Fra <francesco.gaudino@gmail.com>
2023-05-03 16:35:27 +0200

    modifica startup per dotnet watch run

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -20,7 +21,7 @@ namespace MyCourse
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
         {
             if(env.IsProduction())
             {
@@ -29,6 +30,12 @@ namespace MyCourse
             if (env.IsEnvironment("Development"))
             {
                 app.UseDeveloperExceptionPage();
+                lifetime.ApplicationStarted.Register(()=>
+                {
+                    string filePath = Path.Combine(env.ContentRootPath,"bin/reload.txt" );
+                    File.WriteAllText(filePath,DateTime.Now.ToString());
+                });
+
             }
             app.UseStaticFiles();
 
