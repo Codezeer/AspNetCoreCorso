dffd57cdfe5b2d72c6d0b441d5f9a9b56168a41e
Francesco <f.gaudino@almaviva.it>
2023-06-30 18:25:34 +0200

    66. Estrarre l'elenco dei corsi da un servizio applicativo

---
+++ Models/Services/Application/EFCoreCourseService.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyCourse.Models.Services.Infrastructure;
+using MyCourse.Models.ViewModels;
+
+namespace MyCourse.Models.Services.Application
+{
+    public class EFCoreCourseService : ICourseService
+    {
+        private readonly MyCourseDbContext dbContext;
+
+        public EFCoreCourseService(MyCourseDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public Task<CourseDetailViewModel> GetCourseByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<CourseViewModel>> GetCoursesAsync()
+        {
+            List<CourseViewModel> courses = await dbContext.Courses.Select(course => 
+            new CourseViewModel{
+                Id = course.Id,
+                Title = course.Title,
+                ImagePath = course.ImagePath,
+                Author = course.Author,
+                Rating = course.Rating,
+                CurrentPrice = course.CurrentPrice,
+                FullPrice = course.FullPrice
+            }).ToListAsync();
+
+            return courses;
+        }
+    }
+}
